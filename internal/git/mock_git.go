f8e07a729434b7a8a4a639eeac1af617ce5f0fa7
Tim Hughes <thughes@thegoldfish.org>
2025-03-26 23:03:48 +0000

    fixing tests

--- internal/git/mock_git.go
+++ internal/git/mock_git.go
@@ -2,58 +2,58 @@ package git
 
 // MockGitClient implements GitClient for testing
 type MockGitClient struct {
-	CommitCalled   bool
-	CommitError    error
-	HasRemoteValue bool
-	InitError      error
-	IsRepoValue    bool
-	LastCommitMsg  string
-	LastCommitPath string
-	PullCalled     bool
-	PullError      error
-	PushCalled     bool
-	PushError      error
-	StatusError    error
-	StatusValue    string
+IsRepoValue     bool
+HasRemoteValue  bool
+StatusValue     string
+StatusError     error
+InitError       error
+CommitError     error
+PullError       error
+PushError       error
+CommitCalled    bool
+PullCalled      bool
+PushCalled      bool
+LastCommitPath  string
+LastCommitMsg   string
 }
 
 func NewMockGitClient() *MockGitClient {
-	return &MockGitClient{
-		IsRepoValue:    true,
-		HasRemoteValue: true,
-		StatusValue:    "",
-	}
+return &MockGitClient{
+IsRepoValue:    true,
+HasRemoteValue: true,
+StatusValue:    "", // Empty string means clean status
+}
 }
 
 func (m *MockGitClient) IsRepository(path string) bool {
-	return m.IsRepoValue
+return m.IsRepoValue
 }
 
 func (m *MockGitClient) HasRemote(path string) bool {
-	return m.HasRemoteValue
+return m.HasRemoteValue
 }
 
 func (m *MockGitClient) Status(path string) (string, error) {
-	return m.StatusValue, m.StatusError
+return m.StatusValue, m.StatusError
 }
 
 func (m *MockGitClient) Init(path string) error {
-	return m.InitError
+return m.InitError
 }
 
 func (m *MockGitClient) Commit(path, message string) error {
-	m.CommitCalled = true
-	m.LastCommitPath = path
-	m.LastCommitMsg = message
-	return m.CommitError
+m.CommitCalled = true
+m.LastCommitPath = path
+m.LastCommitMsg = message
+return m.CommitError
 }
 
 func (m *MockGitClient) Pull(path string) error {
-	m.PullCalled = true
-	return m.PullError
+m.PullCalled = true
+return m.PullError
 }
 
 func (m *MockGitClient) Push(path string) error {
-	m.PushCalled = true
-	return m.PushError
+m.PushCalled = true
+return m.PushError
 }
